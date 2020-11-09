eb78f453de2fd438f9050029df7cab63f9c6cbae
Osong Agberndifor <agberndifor7@gmail.com>
2020-11-10 00:38:44 +0100

    Add style to todo items

--- src/TodoList/TodoItem.js
+++ src/TodoList/TodoItem.js
@@ -4,13 +4,25 @@ import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles({
   root: {
-    height: '3em',
+    height: 'auto',
+    margin: '1em',
+    padding: '2em'
+    
+  },
+
+  paper: {
+    margin: '1em',
     padding: '2em',
-    margin: '1em'
+    textAlign: 'justify'
+  },
+
+  heading: {
+    textAlign: 'center'
   }
 })
 
@@ -22,9 +34,14 @@ const TodoItem = (props) => {
   }
   return (
     <Grid container spacing={0}>
-      <Grid item xs={9}>
-        <Paper elevation={3} className={classes.root}>
-          {props.item.task}
+    <Grid item xs={1} />
+      <Grid item xs={10}>
+        <Paper elevation={3} className={classes.paper}>
+          <Typography variant="h4" className={classes.heading}>
+            {props.item.task}
+          </Typography>
+          <hr />
+          {props.item.body}
           <hr />
           <Button
             variant="contained"
